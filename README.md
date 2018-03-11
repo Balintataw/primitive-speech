d84b5d91f35694278e030ecfba4578945c2a1f84
balintataw <balintataw2@gmail.com>
2018-03-11 10:02:37 -0700

    changed repo

---
+++ README.md
@@ -0,0 +1,10 @@
+<a href="https://github.com/Balintataw/ssp19-twittler"><img src="./resources/images/readmeScreenshot.PNG" style="max-width:100%;"/></a> 
+A recently discovered form of communication used by the Norse men of old. Eerily reminiscent of Twitter.
+
+Check it out here: https://balintataw.github.io/ssp19-twittler/
+
+Future plans : 
+    <FIXED>Fix 'Go Back' button functionality to re add filtered tweets rather than refresh the page
+    <FIXED>After user tweet is added, select user by click breaks. It still works by search.
+    <ISSUE>There is a 1 second delay in time stamp loading. 
+    <ISSUE>users username cannot be selected to show feed of only users tweets by name click, only by name search.
---
+++ data_generator.js
@@ -0,0 +1,69 @@
+/*
+ * NOTE: This file generates fake tweet data, and is not intended to be part of your implementation.
+ * You can safely leave this file untouched, and confine your changes to index.html.
+ */
+
+// set up data structures
+window.streams = {};
+streams.home = [];
+streams.users = {};
+streams.users.shawndrost = [];
+streams.users.sharksforcheap = [];
+streams.users.mracus = [];
+streams.users.douglascalhoun = [];
+window.users = Object.keys(streams.users);
+
+// utility function for adding tweets to our data structures
+var addTweet = function(newTweet){
+  var username = newTweet.user;
+  streams.users[username].push(newTweet);
+  streams.home.push(newTweet);
+};
+
+// utility function
+var randomElement = function(array){
+  var randomIndex = Math.floor(Math.random() * array.length);
+  return array[randomIndex];
+};
+
+// random tweet generator
+var opening = ['just', '', '', '', '', 'ask me how i', 'completely', 'nearly', 'productively', 'efficiently', 'last night i', 'the Gods', 'the Gods', 'that shaman', 'an enemy', 'a seedy old man'];
+var verbs = ['destroyed', 'hunted', 'burnt', 'developed', 'built', 'invented', 'experienced', 'navigated', 'aided', 'enjoyed', 'crafted', 'installed', 'sailed', 'delegated', 'attacked', 'fornicated', 'overhauled'];
+var objects = ['my', 'your', 'the', 'a', 'my', 'an entire', 'this', 'that', 'the', 'the big', 'a new form of'];
+var nouns = ['Yggdrasil', 'heavens', 'system', 'city', 'worm', 'cloud', 'potato', 'way of life', 'belief system', 'world tree', 'bad decision', 'future', 'life', 'horse', 'mind', 'mead'];
+var tags = ['#Valhalla', '#burning men', '#norge', 'but only the gods know how', 'for real', '#sxsw', '#ballin', '#god', '#yolo', '#magic', '', '', '', ''];
+
+var randomMessage = function(){
+  return [randomElement(opening), randomElement(verbs), randomElement(objects), randomElement(nouns), randomElement(tags)].join(' ');
+};
+
+// generate random tweets on a random schedule
+var generateRandomTweet = function(){
+  var tweet = {};
+  tweet.user = randomElement(users);
+  tweet.message = randomMessage();
+  tweet.created_at = new Date();
+  addTweet(tweet);
+};
+
+for(var i = 0; i < 20; i++){
+  generateRandomTweet();
+}
+
+var scheduleNextTweet = function(){
+  generateRandomTweet();
+  setTimeout(scheduleNextTweet, Math.random() * 1500);
+};
+scheduleNextTweet();
+
+// utility function for letting students add "write a tweet" functionality
+// (note: not used by the rest of this file.)
+var writeTweet = function(message){
+  if(!visitor){
+    throw new Error('set the global visitor property!');
+  }
+  var tweet = {};
+  tweet.user = visitor;
+  tweet.message = message;
+  addTweet(tweet);
+};
